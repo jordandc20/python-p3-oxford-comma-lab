4489ea058ba34712e0f6f399cf430e039b7402a4
Diana Jordan <dianacjordan@gmail.com>
2023-04-08 20:40:04 -0400

    compelte lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) >1:
+        items[-1] = 'and ' + items[-1]
+
+    if len(items) >2:
+        items= ", ".join(items)
+    else:
+        items= " ".join(items)
+    
+    print(items)
+    return items
